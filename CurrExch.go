ceb6b068f56feea36d9ab80b0f99ead670fcac01
AimeeShanleyBrookes <55086844+AimeeShanleyBrookes@users.noreply.github.com>
2019-09-10 15:25:20 +0100

    CurrExch trial
    Getting to grips with go coding

---
+++ CurrExch.go
@@ -0,0 +1,48 @@
+package main
+
+import(
+        "fmt" 
+        "math"
+      )
+func main() 
+{
+
+  //declarations
+  //stores options
+
+  var options, amount, euro, dollar, euro2, gbd2, dtotal, ptotal int
+  var ans, choice int
+
+  //core program
+
+  //welcome
+
+  fmt.Println("Welcome to the currency exchange program")
+  fmt.Println("Enter 1 if you want to convert euro to dollar")
+  fmt.Println("Enter 2 if you want to convert euro to pounds")
+
+  fmt.Println("Enter your choice:")
+
+  scanf("%d", &choice)
+  get char()
+  
+  fmt.Println("Enter the amount of euro:")
+  scanf("%f", &amount)
+  
+  if (choice ==1)
+    {
+      get char()
+  
+      fmt.Println("%.1f euro(s)is equivalent to %.2f dollar(s)", amount, amount*0.693)
+    }
+  getchar()
+  
+  for(amount=1; amount<=10;amount++)
+    {
+      fmt.Println("%.1f euro(s) is equivalent to %.2f gbp and %.2f usd\n", amount, amount*1.293")
+  
+      fmt.Println("Goodbye\n")
+    }
+  }
+  
+  
