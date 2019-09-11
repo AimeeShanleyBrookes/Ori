f75e0cc00fa0ea8838ac61a09297dd57984fcc09
AimeeShanleyBrookes <55086844+AimeeShanleyBrookes@users.noreply.github.com>
2019-09-11 15:40:11 +0100

    CurrExch function now repeats itself

--- CurrExch.go
+++ CurrExch.go
@@ -8,29 +8,34 @@ const eurotodollars = 1.1
 const eurotopounds = 0.89
 
 func main() {
+	for{
 
-	var choice int
-	var euro float64
+		var choice int
+		var euro float64
 
-	fmt.Println("Welcome")
-	fmt.Println("Enter 1 for euro to dollars")
-	fmt.Println("Enter 2 for euro to pounds")
+		fmt.Println("Welcome")
+		fmt.Println("Enter 1 for euro to dollars")
+		fmt.Println("Enter 2 for euro to pounds")
+		fmt.Println("Enter 3 for the function to terminate!!")
 
-	fmt.Println("Enter choice:")
-	fmt.Scanf("%d", &choice)
+		fmt.Println("Enter choice:")
+		fmt.Scanf("%d", &choice)
+		
+		
+		fmt.Println("Enter the amount of euro:")
+		fmt.Scanf("%f", &euro)
 
-	fmt.Println("Enter the amount of euro:")
-	fmt.Scanf("%f", &euro)
-
-	if choice == 1 {
-		dollars := euro * eurotodollars
-		fmt.Println("dollars", dollars)
-	} else if choice == 2 {
-		pound := euro * eurotopounds
-		fmt.Println("pounds", pound)
-	} else {
-		fmt.Println("Error")
+		if choice == 1 {
+			dollars := euro * eurotodollars
+			fmt.Println("dollars", dollars)
+		} else if choice == 2 {
+			pound := euro * eurotopounds
+			fmt.Println("pounds", pound)
+		} else {
+			fmt.Println("Goodbye")
+			break;
+			
+		
 	}
-
-	fmt.Println("Goodbye")
+}
 }
