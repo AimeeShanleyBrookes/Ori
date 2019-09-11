2da57e4ba4f151558a66945d3ebcc0519d82a807
AimeeShanleyBrookes <55086844+AimeeShanleyBrookes@users.noreply.github.com>
2019-09-11 17:32:13 +0100

    Currency2.go has been updated

---
+++ Currency2.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Calculate(euro float64) (result float64) {
+	result = euro * 0.89
+	return result
+}
+
+func main(){
+	fmt.Println("Go Testing Tutorial")
+	result := Calculate(2)
+	fmt.Println(result)
+}
